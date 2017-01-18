517f267f5daa7a254f5c091604e8ce94cc76ca69
Bud Wortham <Bud Wortham>
2017-01-18 11:52:30 -0700

    Adding Survey

---
+++ web/header.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="homePage.html">Bud Wortham</a>
+				</div>
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="homePage.html">Home</a></li>
+					<li><a href="assignments.html">Assignments</a></li>
+					<li><a href="about.html">About</a></li>
+				</ul>
+			</div>
+		</nav>
