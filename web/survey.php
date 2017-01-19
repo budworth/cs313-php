0cfbef9856513ba22dd6606fdccde6a5924cb7b4
Bud Wortham <Bud Wortham>
2017-01-19 15:22:28 -0700

    Chaged Survey to list

--- web/survey.php
+++ web/survey.php
@@ -2,22 +2,25 @@
 	<div class="container">
 		<form class="questions" action="survey.php" name="survey" method="POST">
 			Who would win in a fight?
-			<br>
-			<input type="radio" name="fight" value="taco">Tacor<br>
-			<input type="radio" name="fight" value="grilled cheese">Grilled Cheese<br>
+			<ul class="questionList">
+				<li><input type="radio" name="fight" value="taco">Tacor</li>
+				<li><input type="radio" name="fight" value="grilled cheese">Grilled Cheese</li>
+			</ul>
 			
 			What kind of person are you?
-			<br>
-			<input type="radio" name="person" value="Dog">Dog<br>
-			<input type="radio" name="person" value="Cat">Cat<br>
-			<input type="radio" name="person" value="Vulcan">Vulcan<br>
+			<ul class="questionList">
+				<li><input type="radio" name="person" value="Dog">Dog</li>
+				<li><input type="radio" name="person" value="Cat">Cat</li>
+				<li><input type="radio" name="person" value="Vulcan">Vulcan</li>
+			</ul>
 			
 			Guess.
-			<input type="radio" name="guess" value="Heads">Heads or
-			<input type="radio" name="guess" value="Tails">Tails<br>
-			
+			<ul class="questionList">
+				<li><input type="radio" name="guess" value="Heads">Heads</li>
+				<li><input type="radio" name="guess" value="Tails">Tails</li>
+			</ul>
 			Which one is the best?
-			<ul>
+			<ul class="questionList">
 				<li><input type="radio" name="starTrek" value="original">Star Trek: The Original Series</li>
 				<li><input type="radio" name="starTrek" value="generation">Star Trek: The Next Generation</li>
 				<li><input type="radio" name="starTrek" value="ds9">Star Trek: Deep Space Nine</li>
