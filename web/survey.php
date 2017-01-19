c5ff42f67aca86413ca6b948d6d5d83ab1304a18
Bud Wortham <Bud Wortham>
2017-01-19 15:13:39 -0700

    Question Styles

--- web/survey.php
+++ web/survey.php
@@ -2,12 +2,14 @@
 	<div class="container">
 		<form class="questions" action="survey.php" name="survey" method="POST">
 			Who would win in a fight?
-			<input type="radio" name="fight" value="taco">Tacor or
+			<br>
+			<input type="radio" name="fight" value="taco">Tacor<br>
 			<input type="radio" name="fight" value="grilled cheese">Grilled Cheese<br>
 			
 			What kind of person are you?
-			<input type="radio" name="person" value="Dog">Dog or
-			<input type="radio" name="person" value="Cat">Cat or
+			<br>
+			<input type="radio" name="person" value="Dog">Dog<br>
+			<input type="radio" name="person" value="Cat">Cat<br>
 			<input type="radio" name="person" value="Vulcan">Vulcan<br>
 			
 			Guess.
@@ -15,10 +17,12 @@
 			<input type="radio" name="guess" value="Tails">Tails<br>
 			
 			Which one is the best?
-			<input type="radio" name="starTrek" value="original">Star Trek: The Original Series or
-			<input type="radio" name="starTrek" value="generation">Star Trek: The Next Generation or
-			<input type="radio" name="starTrek" value="ds9">Star Trek: Deep Space Nine or
-			<input type="radio" name="starTrek" value="Voyager">Star Trek: Voyager
+			<ul>
+				<li><input type="radio" name="starTrek" value="original">Star Trek: The Original Series</li>
+				<li><input type="radio" name="starTrek" value="generation">Star Trek: The Next Generation</li>
+				<li><input type="radio" name="starTrek" value="ds9">Star Trek: Deep Space Nine</li>
+				<li><input type="radio" name="starTrek" value="Voyager">Star Trek: Voyager</li>
+			</ul>
 			<br>
 				
 		</form>
