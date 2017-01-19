598341248cf01bd58d8b18e48ab63d8f56ddd5b7
Bud Wortham <Bud Wortham>
2017-01-19 15:06:41 -0700

    Added questions and style

--- web/survey.php
+++ web/survey.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
 	<div class="container">
-		<form action="survey.php" name="survey" method="POST">
+		<form class="questions" action="survey.php" name="survey" method="POST">
 			Who would win in a fight?
 			<input type="radio" name="fight" value="taco">Tacor or
 			<input type="radio" name="fight" value="grilled cheese">Grilled Cheese<br>
@@ -10,7 +10,17 @@
 			<input type="radio" name="person" value="Cat">Cat or
 			<input type="radio" name="person" value="Vulcan">Vulcan<br>
 			
+			Guess.
+			<input type="radio" name="guess" value="Heads">Heads or
+			<input type="radio" name="guess" value="Tails">Tails<br>
 			
+			Which one is the best?
+			<input type="radio" name="starTrek" value="original">Star Trek: The Original Series or
+			<input type="radio" name="starTrek" value="generation">Star Trek: The Next Generation or
+			<input type="radio" name="starTrek" value="ds9">Star Trek: Deep Space Nine or
+			<input type="radio" name="starTrek" value="Voyager">Star Trek: Voyager
+			<br>
+				
 		</form>
 		</div>
 <?php include 'footer.php'; ?>
