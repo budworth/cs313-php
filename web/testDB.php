9902054bdf17be911aea10556861736f98895880
Bud Wortham <wor07004@gmail.com>
2017-02-03 07:38:26 -0700

    Trying team activity way

--- web/testDB.php
+++ web/testDB.php
@@ -1,29 +1,38 @@
-<?php    
-    include 'header.php';
-    require 'dbConnect.php';
-
-    $db = get_db();
-?>
+<?php include 'header.php'; ?>
 
 <div class="container">
+        <table border="0" cellspacing="0" cellpadding="0"> 
+            <tr> 
+                <td> 
+                    Friend ID 
+                </td> 
+                <td> 
+                    First Name 
+                </td> 
+                <td> 
+                    Surname 
+                </td> 
+                <td> 
+                    Email Address 
+                </td> 
+            </tr> 
+        <?php 
+        $db = pg_connect('host=ec2-54-243-253-17.compute-1.amazonaws.com dbname=dc3qo0omk1t54q user=nhdhvzjtzgcbvw password=31eefdffa2448f164d09f84d772dc775724c18004b049ef92b42400f9f7762bd');
 
-    <h1>Test</h1>
+        $query = "SELECT * FROM Monsters"; 
 
-    <?php
+        $result = pg_query($query); 
+        if (!$result) { 
+            echo "Problem with query " . $query . "<br/>"; 
+            echo pg_last_error(); 
+            exit(); 
+        } 
 
-        $statement = $db->prepare("SELECT monster_name FROM Monster");
-        $statement->execute();
-        // Go through each result
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-        {
-	       // The variable "row" now holds the complete record for that
-	       // row, and we can access the different values based on their
-	       // name
-	       echo '<p>';
-	       echo '<strong>' . $row['monster_name'];
-        }
-
-    ?>
+        while($myrow = pg_fetch_assoc($result)) { 
+            printf ("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $myrow['id'], htmlspecialchars($myrow['monster_name']), htmlspecialchars($myrow['class_id']), htmlspecialchars($myrow['monster_description']));
+        } 
+        ?> 
+        </table> 
 </div>
 
 <?php include 'footer.php'?>
