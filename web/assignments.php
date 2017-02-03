54fc06b4330e5c069f5c4bc01318f63b683e4a3c
Bud Wortham <wor07004@gmail.com>
2017-02-02 19:19:57 -0700

    Test DB in php

--- web/assignments.php
+++ web/assignments.php
@@ -12,8 +12,8 @@
 			<li>Week 01</li>
 			<li>Week 02</li>
 			<li><a href="survey.php">Week 03: Survey Assignment</a></li>
-			<li>Week 04</li>
-			<li>Week 05</li>
+            <li><a href="dbConnect.php">Week 04: Database</a>a></li>
+			<li><a href="dbConnect.php">Week 05: Database</a>a></li>
 			<li>Week 06</li>
 			<li>Week 07</li>
 			<li>Week 08</li>
---
+++ web/testDB.php
@@ -0,0 +1,22 @@
+<?php 
+    
+    include 'header.php';
+    require 'dbConnect.php';
+
+    $db = get_db();
+
+    $statement = $db->prepare("SELECT * FROM Monster");
+    $statement->execute();
+    // Go through each result
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+    {
+	   // The variable "row" now holds the complete record for that
+	   // row, and we can access the different values based on their
+	   // name
+	   echo '<p>';
+	   echo '<strong>' . $row['monster'];
+    }
+
+    include 'footer';
+?>
+
