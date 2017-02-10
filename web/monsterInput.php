728c015a5022b4ef4c2c8937166007abd36c6d95
Bud Wortham <wor07004@gmail.com>
2017-02-10 12:17:35 -0700

    Testing form

--- web/monsterInput.php
+++ web/monsterInput.php
@@ -2,7 +2,15 @@
 <?php include 'dbConnect.php'; ?>
 
 <div class="container">
-    <h1>Test</h1>
+    <h1>Insert Monster</h1>
+    
+    <form id="monsterForm" action="insertMonster.php" method="POST">
+    
+        <input type="text" name="monster_name"></input>
+        <label for="monster_name">Monster Name</label><br><br>
+    
+        
+    </form>
     
 </div>
 
