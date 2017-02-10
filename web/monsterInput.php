45946e98074d591bd6b3610e381673dd105ae592
Bud Wortham <wor07004@gmail.com>
2017-02-10 12:29:44 -0700

    Testing dropdown select

--- web/monsterInput.php
+++ web/monsterInput.php
@@ -11,7 +11,9 @@
         <br><br>
     
         <select name="class">
-    
+            while($myrow = pg_fetch_assoc($result)){
+        echo '<option value="' . $myrow['monster_name'] . '"> . $myrow['monster_name'];
+        }
         </select>
     </form>
     
