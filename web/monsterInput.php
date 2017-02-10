6bfb2ce6692ff6b943346041225e96483ca362bb
Bud Wortham <wor07004@gmail.com>
2017-02-10 12:22:05 -0700

    Testing dropdown select

--- web/monsterInput.php
+++ web/monsterInput.php
@@ -11,22 +11,8 @@
         <br><br>
     
         <select name="class">
-    <?php
-        $query = 'SELECT monster_class FROM class'; 
-
-        $result = pg_query($query); 
-        if (!$result) { 
-            echo "Problem with query " . $query . "<br/>"; 
-            echo pg_last_error(); 
-            exit(); 
-        }
-
-        while($myrow = pg_fetch_assoc($result)) { 
-            
-            printf ("<select name="monster_name"><option value="$s">$s<option></select>", htmlspecialchars($myrow['monster_name']), htmlspecialchars($myrow['monster_name']));
-        } 
-    ?>
-    </select>
+    
+        </select>
     </form>
     
 </div>
