b0c5aecdbed0cd708b31959b850e89fa1ffef163
Bud Wortham <Bud Wortham>
2017-02-11 10:09:59 -0700

    Added entry fields to form

--- web/monsterInput.php
+++ web/monsterInput.php
@@ -9,10 +9,51 @@
         <label for="monster_name">Monster Name</label>
         <input type="text" name="monster_name"></input>
         <br><br>
+		
+		<label for="hp">Monster HP</label>
+        <input type="number" name="hp"></input>
+        <br><br>
+		
+		<label for="mp">Monster MP</label>
+        <input type="number" name="mp"></input>
+        <br><br>
+		
+		<label for="ac">Monster Armor Class</label>
+        <input type="number" name="ac"></input>
+        <br><br>
+		
+		<label for="init">Monster Initiative</label>
+        <input type="number" name="init"></input>
+        <br><br>
+		
+		<label for="str">Monster Strength</label>
+        <input type="number" name="str"></input>
+        <br><br>
+		
+		<label for="dex">Monster Dexterity</label>
+        <input type="number" name="dex"></input>
+        <br><br>
+		
+		<label for="con">Monster Constitution</label>
+        <input type="number" name="con"></input>
+        <br><br>
+		
+		<label for="int">Monster Intelligence</label>
+        <input type="number" name="int"></input>
+        <br><br>
+		
+		<label for="wis">Monster Wisdom</label>
+        <input type="number" name="wis"></input>
+        <br><br>
+		
+		<label for="cha">Monster Charisma</label>
+        <input type="number" name="cha"></input>
+        <br><br>
     
+		<label for="class">Monster Class</label>
         <select name="class">
         <?php
-            $query ='SELECT * FROM monster';
+            $query ='SELECT * FROM class';
 			
 			 $result = pg_query($query); 
 			 
@@ -24,10 +65,11 @@
             
             while($myrow = pg_fetch_assoc($result))
             {
-               printf ("<option value=\"%s\">%s</option>", htmlspecialchars($myrow['monster_name']), htmlspecialchars($myrow['monster_name']));
+               printf ("<option value=\"%s\">%s</option>", htmlspecialchars($myrow['monster_class']), htmlspecialchars($myrow['monster_class']));
             }
         ?>
         </select>
+		<br><br>
     </form>
     
 </div>
